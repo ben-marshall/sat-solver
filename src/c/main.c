73f3a7a1fb5489db35376ffb67931a20e2705514
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-07-09 12:19:51 +0100

    Skeleton C source code and CMake flow.  On branch master  Changes to be committed: 	new file:   CMakeLists.txt 	new file:   Makefile 	new file:   imp-matrix.c 	new file:   imp-matrix.h 	new file:   main.c 	new file:   satsolver.h

---
+++ src/c/main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+/*!
+@brief The Main entry point function for the wrapper program.
+@parameter [in] argc - Number of input arguments
+@parameter [in] argv - Values of input arguments.
+@returns 0 If the program terminates successfully, otherwise 1 to indicate
+         an error.
+*/
+int main (int argc, char ** argv) 
+{
+    printf("----------[SAT-Solver]----------\n");
+
+    return 0;
+}
