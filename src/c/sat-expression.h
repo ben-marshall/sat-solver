ac6084f3176c410a5de0da232f350d03a6345226
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-07-09 16:45:09 +0100

    Skeleton files for representing expressions.  On branch master  Changes to be committed: 	new file:   ../src/c/sat-expression.c 	new file:   ../src/c/sat-expression.h

---
+++ src/c/sat-expression.h
@@ -0,0 +1,17 @@
+
+#ifndef H_SATEXPRESSION
+#define H_SATEXPRESSION
+
+//! @typedef Expression Variable ID.
+typedef char * sat_var_id;
+
+/*!
+@brief Describes a single binary expression
+*/
+typedef struct t_sat_binary_expression {
+    
+} sat_expression;
+
+
+#endif
+
