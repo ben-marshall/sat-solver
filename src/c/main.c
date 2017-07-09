1e371cce816236e31db818f81fccf2b7095c2acb
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-07-09 18:24:16 +0100

    Implement insertion of AND/OR/NOT implications.  On branch master  Changes to be committed: 	modified:   ../src/c/main.c

--- src/c/main.c
+++ src/c/main.c
@@ -16,7 +16,63 @@ void print_usage(char * command_line) {
                              variables to be used in the expression.");
     printf("\n");
 }
+ 
 
+/*!
+@brief Adds parsed expressions to the implication matrix.
+*/
+void add_expressions(
+    sat_imp_matrix * imp_mat
+) {
+    unsigned long expression_count = 0;
+
+    sat_binary_expression * walker = yy_expressions;
+
+    sat_imp_matrix_cell * as_l;
+    sat_imp_matrix_cell * as_r;
+    sat_imp_matrix_cell * l_as;
+    sat_imp_matrix_cell * r_as;
+
+    as_l = sat_get_imp_matrix_cell(imp_mat,walker -> assigne, walker -> lhs);
+    as_r = sat_get_imp_matrix_cell(imp_mat,walker -> assigne, walker -> rhs);
+    l_as = sat_get_imp_matrix_cell(imp_mat, walker -> lhs,walker -> assigne);
+    r_as = sat_get_imp_matrix_cell(imp_mat, walker -> rhs,walker -> assigne);
+
+    while(walker != NULL) {
+
+        switch(walker -> op) {
+            case (OP_AND):
+                sat_set_imp_matrix_cell(as_l,1,0,0,0);
+                sat_set_imp_matrix_cell(as_r,1,0,0,0);
+                sat_set_imp_matrix_cell(l_as,0,0,0,1);
+                sat_set_imp_matrix_cell(r_as,0,0,0,1);
+                break;
+
+            case (OP_OR ):
+                sat_set_imp_matrix_cell(l_as,1,0,0,0);
+                sat_set_imp_matrix_cell(r_as,1,0,0,0);
+                break;
+
+            case (OP_XOR):
+                assert(1==0); // not supported yet.
+                break;
+
+            case (OP_EQ ):
+                assert(1==0); // not supported yet.
+                break;
+
+            case (OP_NOT):
+                sat_set_imp_matrix_cell(as_l,0,1,1,0);
+                sat_set_imp_matrix_cell(l_as,0,1,1,0);
+                break;
+        }
+
+        walker = walker -> next;
+        expression_count += 1;
+    }
+
+    printf("Parsed %lu Expressions into matrix.\n",expression_count);
+}
 
 /*!
 @brief The Main entry point function for the wrapper program.
@@ -69,6 +125,14 @@ int main (int argc, char ** argv)
     printf("[DONE]\n");
 
     printf("Matrix Consistant: %d\n", result -> is_consistant);
+    free(result);
+
+    add_expressions(imp_mat);
+
+    // Do a consistancy check on the filled matrix.
+    printf("Checking full matrix for consistancy...  "); fflush(stdout);
+    result = sat_check_imp_matrix(imp_mat, 1);
+    printf("[DONE]\n");
     
 
     // Free the implication matrix and return.
